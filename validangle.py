5f783845451d9671204aee7198e4849c25fa8c4c
unknown7751 <145188812+unknown7751@users.noreply.github.com>
2025-09-30 19:02:22 +0530

    Added input for third angle and validated sum

--- validangle.py
+++ validangle.py
@@ -1,10 +1,7 @@
 a1=int(input("Enter fist angle : "))
 a2=int(input("Enter second angle : "))
-a3=
-if 
+a3 = int(input("Enter third angle : "))
+if a1 + a2 + a3 == 180:
     print("Valid angles")
 else:
     print("Invalid angles")
-
-
-
